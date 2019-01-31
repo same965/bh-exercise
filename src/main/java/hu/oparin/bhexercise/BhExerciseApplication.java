10a144e53c96013d516653cb40f05e023380b31d
Oparin-Salamon Melinda <melinda@oparin.hu>
2019-01-31 15:51:44 +0100

    init(project): with database connection

---
+++ src/main/java/hu/oparin/bhexercise/BhExerciseApplication.java
@@ -0,0 +1,14 @@
+package hu.oparin.bhexercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BhExerciseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BhExerciseApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/hu/oparin/bhexercise/BhExerciseApplicationTests.java
@@ -0,0 +1,17 @@
+package hu.oparin.bhexercise;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BhExerciseApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
